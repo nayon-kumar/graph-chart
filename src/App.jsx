a368b9a01219acc1ed53da708945ed14519f74f8
nayon-kumar <nayon.cse55@gmail.com>
2026-03-19 12:30:14 +0600

    Straight Angle Pie Chart added

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import ResultChart from "./components/ResultChart/ResultChart";
 import MarksChart from "./components/MarksChart/MarksChart";
 import axios from "axios";
 import ResultArea from "./components/ResultArea/ResultArea";
+import StraightGraph from "./components/StraightGraph/StraightGraph";
 
 const marksPromise = fetch("marks.json").then((res) => res.json());
 const allMarksPromise = axios.get("allMarks.json");
 const resultAreaPromise = axios.get("marks.json");
+const straightPromise = axios.get("straight.json");
 
 function App() {
   return (
@@ -39,6 +41,15 @@ function App() {
           }
         >
           <ResultArea resultAreaPromise={resultAreaPromise}></ResultArea>
+        </Suspense>
+        <Suspense
+          fallback={
+            <div className="flex items-center justify-center mt-30">
+              <span className="loading loading-spinner loading-xl"></span>
+            </div>
+          }
+        >
+          <StraightGraph straightPromise={straightPromise}></StraightGraph>
         </Suspense>
       </main>
     </>
---
+++ src/components/StraightGraph/StraightGraph.jsx
@@ -0,0 +1,32 @@
+import { RechartsDevtools } from "@recharts/devtools";
+import React, { use } from "react";
+import { Pie, PieChart } from "recharts";
+
+const StraightGraph = ({ straightPromise }) => {
+  const straightDataRes = use(straightPromise);
+  const data = straightDataRes.data;
+  return (
+    <div>
+      <h3 className="text-2xl mt-10 -mb-20 text-center font-bold">
+        Straight Angle Pie Chart
+      </h3>
+      <PieChart className="w-[90%] mx-auto" height={500} responsive>
+        <Pie
+          dataKey="value"
+          startAngle={180}
+          endAngle={0}
+          data={data}
+          cx="50%"
+          cy="100%"
+          outerRadius="120%"
+          fill="#8884d8"
+          label
+          isAnimationActive={true}
+        />
+        <RechartsDevtools />
+      </PieChart>
+    </div>
+  );
+};
+
+export default StraightGraph;
